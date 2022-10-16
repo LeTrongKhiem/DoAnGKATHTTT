d74eec4cc586b06d19fb782b588544de4fc146f2
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2022-10-16 07:51:46 +0700

    16-10

---
+++ src/View/SymmetricEncryption.java
@@ -0,0 +1,50 @@
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JSlider;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
+
+public class SymmetricEncryption extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SymmetricEncryption frame = new SymmetricEncryption();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SymmetricEncryption() {
+		setTitle("SymmetricEncryption");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 930, 555);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+	}
+}
